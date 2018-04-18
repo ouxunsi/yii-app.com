47b0e68e467b120bb80d1ca7c8c529668f0f80c3
fuzhuangzhuang <zhuangzhuang.fu@bj.china.com>
2018-04-18 15:13:20 +0800

    资讯样式修改

--- frontend/modules/api/v1/views/article/news-list.php
+++ frontend/modules/api/v1/views/article/news-list.php
@@ -15,7 +15,7 @@ use yii\helpers\Html;
                         <?php endif; ?></a></div>
                 <div class="zxmod3con">
                     <div class="zxmod3tit1"><?php echo Yii::$app->formatter->asDatetime($model->published_at,'php:Y/m/d') ?></div>
-                    <div class="zxmod3tit2" style="font-size: 15.5833px; line-height: 18.7px;">
+                    <div class="zxmod3tit2">
                         <?php echo Html::a($model->title, \yii\helpers\Url::toRoute(['/article/view', 'slug'=>$model->slug])) ?>
                     </div>
                 </div>
